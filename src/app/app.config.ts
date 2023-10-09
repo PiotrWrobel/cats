9b85077533cdf89ee4934e5710ccce16461fbc25
Piotr <44137367+PiotrWrobel@users.noreply.github.com>
2023-10-09 18:53:18 +0200

    pw/feat/adding internalization
    Co-authored-by: Piotr Wróbel <p.j.wrobel@pm.me>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 
+const DEFAULT_LANGUAGE: string = 'pl';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+      HttpClientModule,
+      TranslateModule.forRoot({
+        defaultLanguage: DEFAULT_LANGUAGE,
+        loader: {
+          provide: TranslateLoader,
+          useFactory: translateLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    ])
+  ]
 };
+
+function translateLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
